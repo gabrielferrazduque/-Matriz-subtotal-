5b5f61cda46664083de8a45ec2946ffbe055ec00
Gabriel Ferraz Duque <83476335+gabrielferrazduque@users.noreply.github.com>
2024-03-25 08:10:23 -0300

    Update Matriz-subtotal.c

--- Matriz-subtotal.c
+++ Matriz-subtotal.c
@@ -1,6 +1,8 @@
-#include <stdio.h>  
+ #include <stdio.h> 
+#include<stdlib.h>
 #include<matriz.h>  
-#include<math.h>   
+#include<math.h>         
+#include<time.h>
 void calcularSubtotais(pib,assintes,indotras,fabricas,apis,pais);  
  
  void imprimir_array(imprimir_array int ar[], unsigned int qntd); 
